589c50dd5d936b6a1e2b2e782d1f10ea9504e13c
rishirj <rishirj422@gmail.com>
2024-04-09 22:37:08 +0530

    basic made easy

---
+++ 01vitebasic/src/App.jsx
@@ -0,0 +1,12 @@
+import Custom from "./Custom"
+function App() {
+
+  return (
+    <>
+    <h1>Working</h1>
+    <Custom></Custom>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitebasic/src/Custom.jsx
@@ -0,0 +1,4 @@
+function Custom(){
+    return (<h1>it is custom component made just to try only</h1>)
+}
+export default Custom
